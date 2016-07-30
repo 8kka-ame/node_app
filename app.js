f64409ff6b3391c3ff11a911b5e7dcaf796feaae
8kka-ame <8kka-drop@gmail.com>
2016-07-30 17:59:03 +0900

    add bootstrap setting

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// bootstrapを有効にする
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 app.use('/', routes);
 app.use('/users', users);
 
