33aaa01763a200e260f95564ac2c08198f91bf53
8kka-ame <8kka-drop@gmail.com>
2016-07-30 18:00:33 +0900

    add jquery setting

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // bootstrapを有効にする
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use('/', routes);
